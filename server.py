99f4d7382351bca76ba61cf77dc908b82f4d4b48
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2011-07-10 00:35:02 -0300

    Added document's related functions (new and open)

--- server.py
+++ server.py
@@ -7,10 +7,18 @@ PORT = 4567
 VERBOSE = True
 
 import uuid
+
+class Document:
+    class DoesNotExist(Exception):
+        pass
+
+    def __init__(self, uid):
+        self._uid = uid
+
 class Server(object):
 
     def __init__(self):
-        self.documents = {}
+        self._documents = {}
 
     def register_client(self):
         '''
@@ -19,6 +27,16 @@ class Server(object):
         # TODO save client uid internaly
         client_uid = u'C%s' % uuid.uuid4()
         return client_uid
+
+    def new_document(self, client_uid):
+        document_uid = u'D%s' % uuid.uuid4()
+        self._documents[document_uid] = Document(document_uid)
+        return document_uid
+
+    def open_document(self, client_uid, document_uid):
+        if document_uid in self._documents:
+            return document_uid
+        raise Document.DoesNotExist()
 
 
 if __name__ == '__main__':
--- testAll.py
+++ testAll.py
@@ -1,5 +1,5 @@
 import unittest
-from server import Server
+from server import Server, Document
 
 class TestServerClient(unittest.TestCase):
     '''
@@ -17,6 +17,44 @@ class TestServerClient(unittest.TestCase):
         client1 = server.register_client()
 
         self.failIf(client0 == client1)
+
+class TestServerDocument(unittest.TestCase):
+    '''
+    Test documents' related server functions
+    '''
+    def setUp(self):
+        server = Server()
+        self.server = server
+        self.client0 = server.register_client()
+        self.client1 = server.register_client()
+
+    def test_new_document(self):
+        server = self.server
+        client0 = self.client0
+        document0 = server.new_document(client0)
+        self.assertTrue(document0 in server._documents)
+
+    def test_open_document(self):
+        server = self.server
+        client0 = self.client0
+        client1 = self.client1
+        document0 = server.new_document(client0)
+        document1 = server.open_document(client1, document0)
+        # opened same document
+        self.assertEqual(document0, document1)
+        # and do not create a new document
+        self.assertEqual(len(server._documents.keys()), 1)
+
+    def test_open_document_error(self):
+        server = self.server
+        client0 = self.client0
+        client1 = self.client1
+        document0 = 'D_invalid'
+        try:
+            document1 = server.open_document(client1, document0)
+            self.failIf(True)
+        except Document.DoesNotExist:
+            pass
 
 if __name__ == '__main__':
     unittest.main()
