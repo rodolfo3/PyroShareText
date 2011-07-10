b22d4b3f36d02f18fb75e23a60047b7f06568a74
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2011-07-10 01:36:36 -0300

    Added "write" to a document, working with lists. Can be a problem if use with large files. Added dumb methods to lock and unlock (need more code to work)

--- server.py
+++ server.py
@@ -9,11 +9,30 @@ VERBOSE = True
 import uuid
 
 class Document:
-    class DoesNotExist(Exception):
+    class DoesNotExist(Exception): # cannot find a document
+        pass
+
+    class LockDenied(Exception): # cannot get a lock
         pass
 
     def __init__(self, uid):
         self._uid = uid
+        self.rows = []
+
+    def write(self, row, text):
+        rows = text.strip().split('\n')
+        if len(self.rows) <= row:
+            self.rows.extend(rows)
+        else:
+            all_rows = self.rows
+            # replace 1 line and add in the middle
+            self.rows = all_rows[:row] + rows + all_rows[row+1:]
+
+    def lock(self, client_uid, row):
+        pass
+
+    def unlock(self, client_uid, row):
+        pass
 
 class Server(object):
 
@@ -28,15 +47,29 @@ class Server(object):
         client_uid = u'C%s' % uuid.uuid4()
         return client_uid
 
+    def _get_document(self, uid):
+        if uid in self._documents:
+            return self._documents[uid]
+        raise Document.DoesNotExist()
+
     def new_document(self, client_uid):
         document_uid = u'D%s' % uuid.uuid4()
         self._documents[document_uid] = Document(document_uid)
         return document_uid
 
     def open_document(self, client_uid, document_uid):
-        if document_uid in self._documents:
-            return document_uid
-        raise Document.DoesNotExist()
+        document = self._get_document(document_uid)
+        return document_uid
+
+    def write_document(self, client_uid, document_uid, row, text):
+        document = self._get_document(document_uid)
+        document.write(row, text)
+        document.unlock(client_uid, row)
+        return document_uid
+
+    def lock_document(self, client_uid, document_uid, row):
+        document = self._get_document(document_uid)
+        document.lock(client_uid, row)
 
 
 if __name__ == '__main__':
--- testAll.py
+++ testAll.py
@@ -56,5 +56,89 @@ class TestServerDocument(unittest.TestCase):
         except Document.DoesNotExist:
             pass
 
+    def test_document_write(self):
+        server = self.server
+        client0 = self.client0
+        document0 = server.new_document(client0)
+        server.write_document(client0, document0, 0, 'row 000\nrow 001')
+
+class TestDocument(unittest.TestCase):
+    '''
+    Test document internal methods
+    '''
+    def setUp(self):
+        server = Server()
+        self.client0 = server.register_client()
+        self.document = Document('uid')
+
+    def test_write(self):
+        client0 = self.client0
+        document = self.document
+        document.write(row=0, text='row 000\nrow 001')
+        self.assertEqual(document.rows, ['row 000', 'row 001'])
+
+    def test_second_write(self):
+        client0 = self.client0
+        document = self.document
+        document.rows = ['row 000', 'row 001']
+        document.write(row=2, text='row 002\nrow 003')
+        self.assertEqual(document.rows,
+            ['row 000', 'row 001', 'row 002', 'row 003'])
+
+    def test_write_replace(self):
+        client0 = self.client0
+        document = self.document
+        document.rows = ['row 000', 'row 001']
+
+        document.write(row=0, text='r0')
+        self.assertEqual(document.rows, ['r0', 'row 001'])
+
+    def test_write_addline(self):
+        client0 = self.client0
+        document = self.document
+        document.rows = ['r0', 'r1', 'r4', 'r4']
+
+        document.write(row=2, text='r2\nr3')
+        self.assertEqual(document.rows, ['r0', 'r1', 'r2', 'r3', 'r4'])
+
+
+class TestDocumentEdit(unittest.TestCase):
+    '''
+    Test document's editor methods
+    '''
+
+    def setUp(self):
+        server = Server()
+        server = server
+        client0 = server.register_client()
+        client1 = server.register_client()
+
+        # document with 2 lines
+        document0 = server.new_document(client0)
+        server.write_document(client0, document0, 0, 'row 000\nrow 001')
+
+        server.open_document(client1, document0)
+
+        self.server = server
+        self.client0 = client0
+        self.client1 = client1
+        self.document = document0
+
+    def test_edit_different_paragraphs(self):
+        server = self.server
+        client0 = self.client0
+        client1 = self.client1
+        document0 = self.document
+
+        server.lock_document(client0, document0, 0) # lock document0, row 0
+        server.lock_document(client0, document0, 1) # lock document0, row 1
+
+        server.write_document(client0, document0, 0, 'changed row 0')
+        server.write_document(client1, document0, 1, 'changed row 1')
+
+        # write clear the lock, let's check
+        server.lock_document(client0, document0, 1) # ok, no exception
+
+
 if __name__ == '__main__':
     unittest.main()
